08442024e944d0c61479630f5f6f43ad08138b00
Jeyamurugan Palaniselvam <jyamurugan@gmail.com>
2025-12-09 07:44:01 +0530

    Revert "feat: Add FHIR client for interacting with VSAC API, including value set and code system operations."
    This reverts commit 10e3bcd3c7eaa02524d36c92cb803ba67c9e6521.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { UtilityClient } from './lib/utilityClient.js';
 export { SVSClient } from './lib/svsClient.js';
-export { FHIRClient } from './lib/fhirClient.js';
 export type * from './lib/models/util.js';
 export type * from './lib/models/svs.js';
-export type * from './lib/models/fhir.js';
 export type * from './lib/models/config.js';
